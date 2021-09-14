bf1a0132e50f17e8088917e58cbccac3127eb9ca
wilmer-pachon <31974105+wilmer-pachon@users.noreply.github.com>
2021-09-14 15:17:46 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -5,10 +5,10 @@
     $telefono = $_POST['telefono'] ;
     $mensaje = $_POST['msg'] ;
 
-    $header = "Enviado desde la pagina de Distribuciones porras";
+    $header = "Enviado desde la pagina de Ingestructuras";
     $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
 
     mail($destinatario, $mensaje, $mensajeCompleto, $header);
     echo "<script>alert('correo enviado exitosamente')</script>";
-    echo "<script> setTimeout(\"location.href='Contact_us.html'\",1000)</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
 ?>
